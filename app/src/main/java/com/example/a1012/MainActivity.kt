092e6ff79b708de247ad95e493fad81f81b85c11
rohit <rohit.kabotra@gmail.com>
2023-12-13 11:03:40 +0100

    ordine

--- app/src/main/java/com/example/a1012/MainActivity.kt
+++ app/src/main/java/com/example/a1012/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.a1012
 // MainActivity.kt
 
 
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.widget.Button
@@ -12,41 +13,39 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Build
 import android.view.View
 import android.view.WindowInsetsController
 import android.widget.ProgressBar
-import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.lifecycleScope
 import com.example.a1012.R.*
 import kotlinx.coroutines.*
+import java.util.Calendar
 import java.util.Locale
 import java.util.concurrent.TimeUnit
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var timerTextView: TextView
-    private lateinit var startButton: Button
-    private lateinit var stopButton: Button
     private lateinit var parkButton: Button
-    private lateinit var showMapButton: Button
-    private lateinit var exitButton: Button // Aggiunto il pulsante di uscita
+    private lateinit var exitButton: Button
     private lateinit var locationManager: LocationManager
     private var parkedLocation: Location? = null
     private var timer: CountDownTimer? = null
     private val durationMillis: Long = 30 * 60 * 1000 // 30 minuti
     private val LOCATION_PERMISSION_REQUEST_CODE = 0
     private val mainScope = MainScope()
-    private lateinit var endTimeTextView: TextView
     private var endTimeMillis: Long = 0
+    private lateinit var endTimeTextView: TextView // Dichiarazione di endTimeTextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Inizializzazione di endTimeTextView
+        endTimeTextView = findViewById(R.id.timeRemainingTextView)
+
 
         // Check if the device is running Android 11 or higher
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
@@ -56,16 +55,13 @@ class MainActivity : ComponentActivity() {
             )
         } else {
             // For devices below Android 11
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_VISIBLE
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_VISIBLE
         }
 
-        timerTextView = findViewById(id.timerTextView)
-        startButton = findViewById(id.startButton)
-        stopButton = findViewById(id.stopButton)
-        parkButton = findViewById(id.parkButton)
-        showMapButton = findViewById(id.showMapButton)
-        exitButton = findViewById(id.exitButton)
-        endTimeTextView = findViewById(id.endTimeTextView)
+        // Inizializza le altre variabili
+        parkButton = findViewById(R.id.parkButton)
+        exitButton = findViewById(R.id.exitButton)
 
         // Inizializza il LocationManager
         locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
@@ -105,89 +101,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
-
-    private fun requestLocationPermission() {
-        // Richiedi i permessi
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-            LOCATION_PERMISSION_REQUEST_CODE
-        )
-    }private fun showParkingLocationOnMap() {
-        // Assicurati che la posizione del parcheggio sia stata memorizzata
-        if (parkedLocation != null) {
-            // Costruisci l'URI per aprire Google Maps con la posizione del parcheggio
-            val uri = "geo:${parkedLocation!!.latitude},${parkedLocation!!.longitude}?q=${parkedLocation!!.latitude},${parkedLocation!!.longitude}(Posizione del parcheggio)"
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
-            intent.setPackage("com.google.android.apps.maps")
-
-            // Verifica che l'app di Google Maps sia installata sul dispositivo
-            if (intent.resolveActivity(packageManager) != null) {
-                startActivity(intent)
-            } else {
-                showToast("Google Maps non è installato.")
-            }
-        } else {
-            showToast("Posizione del parcheggio non memorizzata.")
-        }
-    }
-    private fun showDialogWithExplanation() {
-
-    }
-
-    override fun onDestroy() {
-        timer?.cancel() // Cancella il timer se è attivo
-        lifecycleScope.cancel()
-        super.onDestroy()
-    }
-
-    private fun startTimer() {
-        val progressBar: ProgressBar = findViewById(R.id.progressBar)
-        progressBar.visibility = View.VISIBLE
-
-        endTimeTextView.visibility = View.VISIBLE
-        val timeRemainingTextView: TextView = findViewById(R.id.timeRemainingTextView)
-        timeRemainingTextView.visibility = View.VISIBLE // Rendi la TextView visibile
-
-        if (timer == null) {
-            endTimeMillis = System.currentTimeMillis() + durationMillis // Calcola l'orario di fine sosta
-
-            timer = object : CountDownTimer(durationMillis, 1000) {
-                override fun onTick(millisUntilFinished: Long) {
-                    val progress = ((millisUntilFinished.toFloat() / durationMillis) * 100).toInt()
-
-                    // Calcola il tempo rimanente e aggiorna la TextView
-                    val remainingTime = formatTime(millisUntilFinished)
-                    timeRemainingTextView.text = "Tempo rimanente: $remainingTime"
-
-                    progressBar.progress = progress
-                }
-
-                override fun onFinish() {
-                    timerTextView.text = "Tempo scaduto!"
-                    progressBar.progress = 0
-                    progressBar.visibility = View.GONE
-                    endTimeTextView.visibility = View.GONE
-                    timeRemainingTextView.visibility = View.GONE // Nascondi la TextView quando il timer è scaduto
-                }
-            }.start()
-        }
-    }
-
-
-    // Funzione per formattare il tempo rimanente
-    private fun formatTime(millis: Long): String {
-        val hours = TimeUnit.MILLISECONDS.toHours(millis)
-        val minutes = TimeUnit.MILLISECONDS.toMinutes(millis - TimeUnit.HOURS.toMillis(hours))
-        val seconds = TimeUnit.MILLISECONDS.toSeconds(millis - TimeUnit.HOURS.toMillis(hours) - TimeUnit.MINUTES.toMillis(minutes))
-        return String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds)
-    }
-
-    // Listener per ottenere gli aggiornamenti sulla posizione
     private val locationListener: LocationListener = object : LocationListener {
         override fun onLocationChanged(location: Location) {
             // Aggiorna la posizione corrente dell'utente
@@ -206,5 +119,80 @@ class MainActivity : ComponentActivity() {
         override fun onProviderDisabled(provider: String) {
             // Called when the user disables the location provider
         }
+    }
+
+    private fun showTimePickerDialog() {
+        val calendar = Calendar.getInstance()
+        val currentHour = calendar.get(Calendar.HOUR_OF_DAY)
+        val currentMinute = calendar.get(Calendar.MINUTE)
+
+        val timePickerDialog = TimePickerDialog(
+            this,
+            { _, hourOfDay, minute ->
+                // Calcola la durata in millisecondi
+                val selectedDuration = (hourOfDay * 60 + minute) * 60 * 1000
+                startTimer(selectedDuration)
+            },
+            currentHour,
+            currentMinute,
+            true
+        )
+
+        timePickerDialog.show()
+    }
+
+    private fun showDialogWithExplanation() {
+        // Implementa il codice per mostrare una spiegazione al richiedere i permessi
+    }
+
+    private fun startTimer(durationMillis: Int) {
+        // Implementa il codice per avviare il timer con la durata selezionata
+        // Usa la variabile 'durationMillis' come la durata del timer
+
+        val progressBar: ProgressBar = findViewById(id.progressBar)
+        progressBar.visibility = View.VISIBLE
+
+        val endTimeTextView: TextView = findViewById(R.id.timeRemainingTextView)
+
+        endTimeTextView.visibility = View.VISIBLE
+        val timeRemainingTextView: TextView = findViewById(id.timeRemainingTextView)
+        timeRemainingTextView.visibility = View.VISIBLE // Rendi la TextView visibile
+
+        if (timer == null) {
+            endTimeMillis = System.currentTimeMillis() + durationMillis // Calcola l'orario di fine sosta
+
+            timer = object : CountDownTimer(durationMillis.toLong(), 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    val progress = ((millisUntilFinished.toFloat() / durationMillis) * 100).toInt()
+
+                    // Calcola il tempo rimanente e aggiorna la TextView
+                    val remainingTime = formatTime(millisUntilFinished)
+                    timeRemainingTextView.text = "Tempo rimanente: $remainingTime"
+
+                    progressBar.progress = progress
+                }
+
+                override fun onFinish() {
+                    // Implementa le azioni quando il timer è scaduto
+                    progressBar.progress = 0
+                    progressBar.visibility = View.GONE
+                    endTimeTextView.visibility = View.GONE
+                    timeRemainingTextView.visibility = View.GONE // Nascondi la TextView quando il timer è scaduto
+                }
+            }.start()
+        }
+    }
+
+    private fun formatTime(millis: Long): String {
+        val hours = TimeUnit.MILLISECONDS.toHours(millis)
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(millis - TimeUnit.HOURS.toMillis(hours))
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(millis - TimeUnit.HOURS.toMillis(hours) - TimeUnit.MINUTES.toMillis(minutes))
+        return String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds)
+    }
+
+    override fun onDestroy() {
+        timer?.cancel() // Cancella il timer se è attivo
+        lifecycleScope.cancel()
+        super.onDestroy()
     }
 }
